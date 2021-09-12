c1d69399ea3c008a240d9fb624d2c0b53ce1ccde
Amlike-Tz <83519622+Amlike-Tz@users.noreply.github.com>
2021-09-12 08:02:15 -0700

    Update .update.py

--- .update.py
+++ .update.py
@@ -1,1 +1,9 @@
 
+os.system ("clear")
+
+os.system("clear")
+time.sleep(2)
+print ("")
+print (yellow+"Bad-Boys Is Checking For Any Updates first---")
+time.sleep(2)
+os.system ("cd && rm -rf RED-VIRUS && git clone https://github.com/Amlike-Tz/RED-VIRUS.git && cd RED-VIRUS && mv .run.py run.py")
