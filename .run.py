b899e8d5a71f6972b3a34fa67504dafadefee04e
Amlike-Tz <83519622+Amlike-Tz@users.noreply.github.com>
2021-09-12 08:00:31 -0700

    Update .run.py

--- .run.py
+++ .run.py
@@ -36,6 +36,7 @@ def delay_print(s):
         sys.stdout.flush()
         time.sleep(0.06)
 
+   '''
 os.system ("clear")
 
 os.system("clear")
@@ -43,7 +44,10 @@ time.sleep(2)
 print ("")
 print (yellow+"Bad-Boys Is Checking For Any Updates first---")
 time.sleep(2)
-os.system ("cd && rm -rf RED-VIRUS && git clone https://github.com/Amlike-Tz/RED-VIRUS.git && cd RED-VIRUS && mv .run.py run.py")      
+'''
+
+os.system ("python update.py")
+
 
 os.system("rm -rf whatsapp_crush_code.text")
 
